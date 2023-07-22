3ba96767e9f092c48804bfd3d043d959e254af1d
Aum Jhala <28yjhal581g@student.ri.edu.sg>
2023-07-22 10:47:58 +0800

    Initial Commit

---
+++ nationaldayhackathon/nationaldayhackathonApp.swift
@@ -0,0 +1,17 @@
+//
+//  nationaldayhackathonApp.swift
+//  nationaldayhackathon
+//
+//  Created by Jhala family on 22/7/23.
+//
+
+import SwiftUI
+
+@main
+struct nationaldayhackathonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
