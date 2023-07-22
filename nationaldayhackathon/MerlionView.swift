75ed3d19eebcd76665b0d0d17f1af68af37cace8
Aum Jhala <28yjhal581g@student.ri.edu.sg>
2023-07-22 12:31:50 +0800

    Added Map

---
+++ nationaldayhackathon/MerlionView.swift
@@ -0,0 +1,37 @@
+//
+//  MerlionView.swift
+//  nationaldayhackathon
+//
+//  Created by Jhala family on 22/7/23.
+//
+
+import SwiftUI
+
+struct Merlion: View {
+    var body: some View {
+        VStack {
+            Image("merlion")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 200, height: 200)
+            
+            Text("The Merlion")
+                .font(.title)
+                .fontWeight(.bold)
+                .padding()
+            
+            Text("The Merlion, with the head of a lion and the body of a fish, is an iconic symbol of Singapore.")
+                .font(.body)
+                .multilineTextAlignment(.center)
+                .padding()
+            
+        }
+        .padding()
+    }
+}
+
+struct Merlion_Previews: PreviewProvider {
+    static var previews: some View {
+        Merlion()
+    }
+}
